6f1cd4d3a6a711ac26dd0e4aff4279c029b17030
Luca Schädler <Luca Schdler@NB-Luca.edu.ds.fhnw.ch>
2020-03-12 15:29:49 +0100

    test geht nicht mit pull

--- src/minesweeper/MineSweeperView.java
+++ src/minesweeper/MineSweeperView.java
@@ -4,8 +4,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
-import javafx.scene.layout.Border;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
